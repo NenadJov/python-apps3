9e8ef03561371685a2d832586a9e926551b2a6ea
NenadJov <52101094+NenadJov@users.noreply.github.com>
2020-04-11 22:30:00 +0200

    new

---
+++ capturing_video/capture.py
@@ -0,0 +1,39 @@
+import cv2, time
+
+video=cv2.VideoCapture(0)
+
+# za sliki
+"""
+check, frame=video.read()
+
+print(check)
+print(frame)
+
+gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+time.sleep(3)
+#cv2.imshow("Capturing",frame)
+cv2.imshow("Capturing",gray)
+
+cv2.waitKey(0)
+"""
+# za video
+
+a=0
+while True:
+    a=a+1
+    check, frame=video.read()
+
+    print(check)
+    print(frame)
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Capturing",gray)
+
+    key=cv2.waitKey(1)
+
+    if key==ord('q'):
+        break
+print(a)
+
+video.release()
+cv2.destroyAllWindows()
